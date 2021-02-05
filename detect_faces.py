b8f694602c8b3a05b523178f3db3e1699eeda96f
Viljami <77445907+Viljami-git@users.noreply.github.com>
2021-02-05 12:28:02 +0200

    added image loading check

--- detect_faces.py
+++ detect_faces.py
@@ -1,11 +1,12 @@
 import cv2
 import argparse
-import os
+
 
 def extension_check(file, extension, file_use):
     if not file.endswith(extension):
-        raise argparse.ArgumentTypeError('{} file must be of type {}'.format(file_use, extension))
+        raise argparse.ArgumentTypeError('{} file must be type of {}'.format(file_use, extension))
     return file
+
 
 def main():
     parser = argparse.ArgumentParser(description='Detect faces from image and return bounding box of biggest detection')
@@ -14,17 +15,15 @@ def main():
     parser.add_argument('--output', required=True,
                         help='Path to the output text file',
                         type=lambda f: extension_check(f, '.txt', 'Output'))
-
     args = parser.parse_args()
 
-    if not os.path.isfile(args.input):
-        raise FileNotFoundError(f'"{args.input}" does not point to a file')
+    img = cv2.imread(args.input)
+    if img is None:
+        raise FileNotFoundError(f'"{args.input}" does not point to a supported image file or correct path')
 
     frozen_graph = 'frozen_inference_graph.pb'
     text_graph = 'graph.pbtxt'
     net = cv2.dnn.readNetFromTensorflow(frozen_graph, text_graph)
-
-    img = cv2.imread(args.input)
     net_input_width = 240
     net_input_height = 180
 
@@ -46,12 +45,14 @@ def main():
         width = max_x - min_x
         height = max_y - min_y
         if score > 0.3 and width > 60:
-            area = int(width*height)
+            area = int(width * height)
             if area > max_area:
                 max_area = area
                 target_face = [min_x, min_y, max_x, max_y]
+
     with open(args.output, 'w') as file:
         file.write(";".join(str(x) for x in target_face))
+
 
 if __name__ == '__main__':
     main()
