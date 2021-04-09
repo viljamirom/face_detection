9c288cd70b70091e037fdeea3e8c6b3aa68751a8
Viljami <77445907+Viljami-git@users.noreply.github.com>
2021-04-09 15:46:35 +0300

    update readme, detection coords in new scale and rewrites the one output file for the latest detections

--- README.md
+++ README.md
@@ -9,10 +9,12 @@ Dependencies: OpenCV - 4.5.1+ (Used in tests)
 Requires a webcam for the video feed. Documentation of the videocapture can be found [here](https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html#details).
 \
 \
-Output: Desired directory path for the detections. (Must give as argument) 
+--output OUTPUT: Desired directory path for the detection file. (Must give as argument) 
 
-Saves the detections in text (.txt) file with timestamp (DayMonthYear_HoursMinutesSeconds) to the given directory. Creates bounding box coordinates of the biggest detected face in form of: min_x;min_y;max_x;max_y.
-Note the coordinate system:
+Saves the detections in text (.txt) file with default name of "latest_face_detection.txt" to the given directory. Updates/rewrites that file after every second. 
+Creates bounding box coordinates of the biggest detected face in form of: min_x;min_y;max_x;max_y.
+
+Note the coordinate system (scaled from 0.0 to 1.0):
 \
 \
 ![Image](https://i.stack.imgur.com/t4AiI.png)
@@ -27,4 +29,7 @@ optional arguments:
   --output OUTPUT  Path to the output directory
 
  ```
-
+Example usage:
+```
+C:\path\to\face_detection>python detect_faces.py --output path/to/outputdir (or only dir name if in current level)
+ ```
--- detect_faces.py
+++ detect_faces.py
@@ -2,12 +2,11 @@ import cv2
 import argparse
 import time
 import os
-from datetime import datetime
 
 
 def parse_output_file(path, format):
-    time_stamp = datetime.now().strftime("%d%m%Y_%H%M%S")
-    output_file = path+time_stamp+format
+    name = "latest_face_detection"
+    output_file = path+name+format
     return output_file
 
 
@@ -40,7 +39,7 @@ def main():
 
     while True:
         _, img = cap.read()
-        # img = cv2.flip(img, 1)  #might need to mirror the image
+        #img = cv2.flip(img, 1)  # might need to mirror the image
 
         blob = cv2.dnn.blobFromImage(img, size=(net_input_width, net_input_height), swapRB=True, crop=False)
         net.setInput(blob)
@@ -49,25 +48,25 @@ def main():
         rows, cols = img.shape[0:2]
         max_area = 0
 
-        default_target = [int(cols / 3), int(rows / 3), int(2 * cols / 3), int(2 * rows / 3)]   # desired default bounding box
+        default_target = [float(1 / 3), float(1 / 3), float(2 / 3), float(2 / 3)]   # desired default bounding box
         target_face = default_target
 
         for detection in netOut[0, 0, :, :]:
             score = float(detection[2])
-            min_x = int(detection[3] * cols)
-            min_y = int(detection[4] * rows)
-            max_x = int(detection[5] * cols)
-            max_y = int(detection[6] * rows)
-            width = max_x - min_x
-            height = max_y - min_y
+            min_x = float(detection[3])
+            min_y = float(detection[4])
+            max_x = float(detection[5])
+            max_y = float(detection[6])
+            width = int(max_x * cols - min_x * cols)
+            height = int(max_y * rows - min_y * rows)
             if score > 0.3 and width > 60:
                 area = int(width * height)
                 if area > max_area:
                     max_area = area
                     target_face = [min_x, min_y, max_x, max_y]
 
-        # optional video feed from detection
-        cv2.rectangle(img, (target_face[0], target_face[1]), (target_face[2], target_face[3]), (255, 0, 0), 2)
+        # video feed from detection
+        cv2.rectangle(img, (int(target_face[0] * cols), int(target_face[1] * rows)), (int(target_face[2] * cols), int(target_face[3] * rows)), (255, 0, 0), 2)
         cv2.imshow("image", img)
 
         file_to_save = parse_output_file(output_path, ".txt")
@@ -75,13 +74,10 @@ def main():
             file.write(";".join(str(x) for x in target_face))
             file.close()
 
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(1)  # note that if video feed window is not active, program must be stopped manually
         if key & 0xFF == ord('q'):
             break
         time.sleep(1)
-
-    cap.release()
-    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
